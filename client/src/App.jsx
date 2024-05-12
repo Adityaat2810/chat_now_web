929d93f8cff54cf8054b8e2922f78c084fe83c6f
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-12 23:39:41 +0530

    set up folder structure and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{lazy} from 'react'
 import {BrowserRouter as Router,Routes,Route, BrowserRouter} from 'react-router-dom'
+
+//so that all file do not load at wonce without any usecase
+const Home =lazy(
+  ()=>import("./pages/home")
+)
+
+const Login =lazy(
+  ()=>import("./pages/Login")
+)
+
+const Chat =lazy(
+  ()=>import("./pages/Chat")
+)
+
+const Groups =lazy(
+  ()=>import("./pages/Groups")
+)
+
+
 const App = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>}/>
-        <Route path='/about' element={<h1>About</h1>}/>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/chat/:id' element={<Chat/>}/>
+        <Route path='/groups' element={<Groups/>}/>
+
+
       </Routes>
     </BrowserRouter>
   )
